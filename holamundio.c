81fe6a0415adbe8330ec2b03f39e7502aa09ad5a
Jesus <angelhernan12486@gmail.com>
2024-10-08 09:46:53 -0600

    hola

---
+++ holamundio.c
@@ -0,0 +1,8 @@
+#include <stdio.h> // Biblioteca estándar para entrada y salida
+
+int main() {
+    // Imprimir mensaje en la consola
+    printf("Hola, mundo!\n");
+
+    return 0; // Indica que el programa terminó con éxito
+}
