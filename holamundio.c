9df7d0333dcedcabedd28b392ff071393d262fee
Jesus <angelhernan12486@gmail.com>
2024-10-10 09:51:22 -0600

    fress

--- holamundio.c
+++ holamundio.c
@@ -1,8 +1,8 @@
 #include <stdio.h> // Biblioteca estándar para entrada y salida
 
 int main() {
-    //sgd
-    printf("Hola, mundo!\n");
+    
+    printf("Hola!\n");
 
     return 0; // Indica que el programa terminó con éxito
 }
